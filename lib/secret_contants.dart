270f3523dc0e79cc18b8a1b7b5a2fbcf05c32cc5
Alton Maseko <altonmaseko1000@gmail.com>
2024-08-20 19:50:35 +0200

    removed api key

--- lib/secret_contants.dart
+++ lib/secret_contants.dart
@@ -1,4 +1,4 @@
-const String googleMapsApiKey = "AIzaSyBnTM_WAk1MBKT4JaKttBACFLaaUFO6gaM";
+const String googleMapsApiKey = "ADD_YOUR_API_KEY";
 const String baseUrl =
     "https://akai-webapp-chfggua5dkb7g5bu.southafricanorth-01.azurewebsites.net/";
 int selectedIndex = 1;
